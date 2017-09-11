bb2fc8958e0c5917373e39a394cb7f1cf6bd8ea7
matt <mattdlockyer@gmail.com>
2017-09-11 14:46:45 -0700

    localforage caching

--- src/js/routes/wish.js
+++ src/js/routes/wish.js
@@ -4,36 +4,52 @@ export default {
   
   data() {
     return {
-      wishes: [],
+      wishes: {}
     };
   },
   
   created() {
-    //jshint ignore:start
-    let attempts = 0;
-    const limit = 10;
-    const check = async() => {
-      attempts++;
-      const contract = APP.contract;
-      if (!contract) {
-        if (attempts === limit) return;
-        console.log('checking again');
-        setTimeout(check, 500);
-        return;
-      }
-      //get wishes
-      const totalWishes = (await contract.totalWishes.call()).toNumber();
-      console.log('wishes found', totalWishes);
-      for (let i = totalWishes - 1; i >= Math.max(totalWishes - 10, 0); i--) {
-        this.wishes.push(await contract.wishes.call(i));
-      }
-    };
-    check();
-    //jshint ignore:end
+    this.update();
   },
   
   methods: {
-    
+    update() {
+      localforage.getItem('wishereum-wishes').then((wishes) => {
+        this.wishes = wishes || {};
+        this.fetch();
+      });
+    },
+    fetch() {
+      let attempts = 0;
+      const limit = 10;
+      //jshint ignore:start
+      const check = async() => {
+        attempts++;
+        const contract = APP.contract;
+        if (!contract) {
+          if (attempts === limit) return;
+          console.log('checking again');
+          setTimeout(check, 500);
+          return;
+        }
+        //get wishes
+        const totalWishes = (await contract.totalWishes.call()).toNumber();
+        //loop through and fetch each wish from the blockchain, if we don't already have it stored locally
+        
+        //TODO What to do with BigNumber types?
+        
+        for (let i = totalWishes - 1; i >= 0; i--) {
+          if (this.wishes[i] === undefined) this.wishes[i] = await contract.wishes.call(i);
+        }
+        
+        localforage.setItem('wishereum-wishes', this.wishes).then(() => {
+          console.log('stored wishes locally');
+        });
+        this.$forceUpdate();
+      };
+      //jshint ignore:end
+      check();
+    }
   },
   
   template: `
@@ -47,8 +63,9 @@ export default {
           </div>
       
       
-          <div v-for="wish in wishes">
-            <p>{{ wish[0] }} - {{ wish[1] }}</p>
+          <div v-for="(wish, key) in wishes">
+            <p>{{ wish[0] }}</p>
+            <p>{{ wish[1] }}</p>
             <p>{{ wish[2] }}</p>
           </div>
           
