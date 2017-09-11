5541f487aef43c4bb83bad5bfa4e83775fa570e8
matt <mattdlockyer@gmail.com>
2017-09-10 20:11:13 -0700

    network object

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,10 @@ import { getWeb3, getAccounts, getContract } from './web3-utils';
 import wishJSON from '../../build/contracts/Wish.json';
 //import deployer from './tests/deploy-wish';
 
+const network = {
+  4: '0x6fD022CfF6d7512B6E662014662DCd467BBA9BcA'
+};
+
 Vue.use(VueRouter);
 Vue.use(VueMaterial);
 
@@ -19,7 +23,7 @@ const APP = window.APP = {};
 APP.init = async() => {
   getWeb3();
   APP.accounts = await getAccounts();
-  APP.contract = getContract(wishJSON, '0x6fD022CfF6d7512B6E662014662DCd467BBA9BcA');
+  APP.contract = getContract(wishJSON, network[4]);
   //window.deployWish = () => deployer.deploy(wishJSON, APP.accounts[0], 4000000);
 };
 //jshint ignore:end
